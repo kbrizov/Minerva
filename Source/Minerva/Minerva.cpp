b9d28e8455a558990c75dbeffe465b615a5210e7
Christian Rizov <k.b.rizov@gmail.com>
2025-01-14 10:03:38 +0200

    Initial commit.

---
+++ Source/Minerva/Minerva.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Minerva.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Minerva, "Minerva" );
